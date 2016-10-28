cfa5fb237c129fd78e425c9946afafffde4207d2
Slobodan Paunović <spaunovic@heilmannsoftware.de>
2016-10-28 12:57:10 +0200

    Configure rails-worker layers as well

--- recipes/configure.rb
+++ recipes/configure.rb
@@ -1,4 +1,4 @@
-if node[:opsworks][:instance][:layers].include?('rails-app')
+if node[:opsworks][:instance][:layers].include?('rails-app') || node[:opsworks][:instance][:layers].include?('rails-worker')
 
   include_recipe "opsworks_custom_env::restart_command"
   include_recipe "opsworks_custom_env::write_config"
--- recipes/restart_command.rb
+++ recipes/restart_command.rb
@@ -1,15 +1,20 @@
 node[:deploy].each do |application, deploy|
 
   execute "restart Rails app #{application} for custom env" do
-    cwd deploy[:current_path]
-    if node[:opsworks][:rails_stack][:name].eql? "apache_passenger"
-      command "touch #{deploy[:deploy_to]}/current/tmp/restart.txt"
-    elsif node[:opsworks][:rails_stack][:name].eql? "nginx_unicorn"
-      command "#{deploy[:deploy_to]}/shared/scripts/unicorn clean-restart"
+    case node[:opsworks][:instance][:layers]
+    when "rails-app"
+      cwd deploy[:current_path]
+      if node[:opsworks][:rails_stack][:name].eql? "apache_passenger"
+        command "touch #{deploy[:deploy_to]}/current/tmp/restart.txt"
+      elsif node[:opsworks][:rails_stack][:name].eql? "nginx_unicorn"
+        command "#{deploy[:deploy_to]}/shared/scripts/unicorn clean-restart"
+      end
+    when "rails-worker"
+      command "sudo monit restart -g delayed_job_#{application}_group"
     end
+
     user deploy[:user]
 
     action :nothing
   end
-
 end
